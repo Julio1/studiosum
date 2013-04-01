88da8bf281fea9ca26b25331721352fee36b152d
Julio <julio1@outlook.com>
2013-04-01 03:33:43 -0600

    Cambio al crear clase de Fecha

---
+++ Fechas/src/Fecha.java
@@ -0,0 +1,25 @@
+import java.sql.Date;
+
+
+public class Fecha {
+
+	private Date fecha1;
+	private Date fecha2;
+	
+	
+	
+	public Fecha (Date pfecha1, Date pfecha2){
+		
+		fecha1=pfecha1;
+		fecha2=pfecha2;
+						
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- Fechas/src/Main.java
+++ Fechas/src/Main.java
@@ -1,4 +1,103 @@
 
-public class Main {
 
-}
+
+import java.io.*;
+
+public class Main {
+	
+	static BufferedReader in = new BufferedReader (new InputStreamReader (System.in) );
+	static PrintStream out = System.out;
+	
+	public static void main (String [] args) throws IOException{
+		
+	
+		int opc;
+		boolean noSalir = true;
+		
+		do{
+			mostrarMenu();
+			opc = leerOpcion();
+			noSalir = ejecutarAccion(opc);
+		}while(noSalir);
+
+		
+		
+	}
+	
+	static public void mostrarMenu (){
+		
+		out.println("*****************************");
+    	out.println("     MENU DE FECHAS     ");
+    	out.println("*****************************");
+    	
+		out.println("1. Agregar Fecha");
+		out.println("2. Comparar Fecha");
+		out.println("3. Salir");
+
+		
+		out.println();
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	static public int leerOpcion () throws java.io.IOException{
+		
+		
+		out.print("Seleccione su opcion: ");
+		int opcion = Integer.parseInt(in.readLine());
+		out.println();
+
+		return opcion;
+		
+		
+	}
+	
+	
+	static boolean ejecutarAccion(int opcion) throws java.io.IOException{
+		boolean noSalir = true;
+			
+				switch(opcion){
+		
+
+			case 1: //Introducir Fecha
+				
+				break;
+				
+
+			case 2: //Comparar Fecha
+				
+				
+				break;
+				
+		
+			case 3: //Salir
+				
+				noSalir = false;
+				out.println("Gracias");
+				break;
+				
+
+				
+			default: //Cualquier otro valor dado por el usuario se considera invalido
+
+				out.println("Opcion invalida");
+				out.println();
+				break;
+			}
+		
+		
+		return noSalir;
+	}
+	
+	
+	
+	}
+	
+	
+	
+	
